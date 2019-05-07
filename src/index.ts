68753d69a962cafdda0432c6072d9d8d54a82fac
Jefwillems <jefwillems100@gmail.com>
2019-05-07 12:49:30 +0200

    add debug log

--- src/index.ts
+++ src/index.ts
@@ -129,6 +129,7 @@ server.use(restify.plugins.queryParser());
 
 // Listen for incoming activities and route them to your bot for processing.
 server.post('/api/messages', (req, res) => {
+  console.log('New request');
   adapter.processActivity(req, res, async turnContext => {
     // Call bot.onTurn() to handle all incoming messages.
     await bot.onTurn(turnContext);
