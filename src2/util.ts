7dcdcf299810f4ddc5e7caf7c72d37606057f996
Jefwillems <jefwillems100@gmail.com>
2019-06-04 10:26:08 +0200

    add highlight sending

--- src2/util.ts
+++ src2/util.ts
@@ -1,5 +1,6 @@
 import { Activity } from 'botbuilder';
 import { ChannelId } from './models/ChannelIds';
+import * as Turndown from 'turndown';
 
 export function checkNested(obj: any, ...levels: string[]): boolean {
   for (let i = 0; i < levels.length; i += 1) {
@@ -14,4 +15,15 @@ export function checkNested(obj: any, ...levels: string[]): boolean {
 
 export function isFacebook(activity: Activity): boolean {
   return activity.channelId === ChannelId.Facebook;
+}
+export function getBestParagraphForDoc(doc: QueryResponse.Document): string {
+  const bestParagraph = doc.paragraphs.sort((a, b) => {
+    return b.scoreInPercent - a.scoreInPercent;
+  })[0];
+  const td = new Turndown();
+  const p = bestParagraph.highlighting
+    ? bestParagraph.highlighting.join(' ')
+    : bestParagraph.content;
+  const reply = td.turndown(p);
+  return reply;
 }
