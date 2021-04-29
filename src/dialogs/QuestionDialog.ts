c7e40ac5e814317f04e76882e92b10e4259eb941
Jefwillems <jefwillems100@gmail.com>
2021-04-29 14:07:25 +0200

    remove typing indicators

--- src/dialogs/QuestionDialog.ts
+++ src/dialogs/QuestionDialog.ts
@@ -2,34 +2,34 @@ import {
   WaterfallDialog,
   WaterfallStepContext,
   DialogContext,
-  ConfirmPrompt,
-} from 'botbuilder-dialogs';
+  ConfirmPrompt
+} from "botbuilder-dialogs";
 import {
   MessageFactory,
   ActivityTypes,
   CardFactory,
   UserState,
   StatePropertyAccessor,
-  TurnContext,
-} from 'botbuilder';
-import CitynetApi from '../api/CitynetApi';
-import { map, take } from 'lodash';
-import lang from '../lang';
-import conceptMapping from '../lang/conceptMapping';
+  TurnContext
+} from "botbuilder";
+import CitynetApi from "../api/CitynetApi";
+import { map, take } from "lodash";
+import lang from "../lang";
+import conceptMapping from "../lang/conceptMapping";
 
-import { ConfirmTypes } from '../models/ConfirmTypes';
-import { readFileSync } from 'fs';
-import { ChannelId } from '../models/ChannelIds';
-import { FacebookCardBuilder, FacebookCard } from '../models/FacebookCard';
-import nodeFetch from 'node-fetch';
-import * as FormData from 'form-data';
-import * as Turndown from 'turndown';
-import CloudinaryApi from '../api/CloudinaryApi';
-import IOptions from '../models/IOptions';
-import { DocumentErrors } from '../models/DocumentErrors';
+import { ConfirmTypes } from "../models/ConfirmTypes";
+import { readFileSync } from "fs";
+import { ChannelId } from "../models/ChannelIds";
+import { FacebookCardBuilder, FacebookCard } from "../models/FacebookCard";
+import nodeFetch from "node-fetch";
+import * as FormData from "form-data";
+import * as Turndown from "turndown";
+import CloudinaryApi from "../api/CloudinaryApi";
+import IOptions from "../models/IOptions";
+import { DocumentErrors } from "../models/DocumentErrors";
 
 export default class QuestionDialog extends WaterfallDialog {
-  public static readonly ID = 'question_dialog';
+  public static readonly ID = "question_dialog";
   private readonly api: CitynetApi;
   private options: IOptions;
   private readonly docsAccessor: StatePropertyAccessor<
@@ -38,7 +38,7 @@ export default class QuestionDialog extends WaterfallDialog {
   constructor(userState: UserState) {
     super(QuestionDialog.ID);
     this.docsAccessor = userState.createProperty<QueryResponse.QueryResponse>(
-      'resolved_data',
+      "resolved_data"
     );
     this.addStep(this.wantToStartQuestion.bind(this));
     this.addStep(this.handleStart.bind(this));
@@ -52,14 +52,14 @@ export default class QuestionDialog extends WaterfallDialog {
 
   private async wantToStartQuestion(step: WaterfallStepContext) {
     console.log(step.context.activity.text);
-    await step.prompt('confirm_prompt', {
+    await step.prompt("confirm_prompt", {
       choices: [
         { value: ConfirmTypes.POSITIVE },
         { value: ConfirmTypes.NEGATIVE },
-        { value: 'Medewerker' },
+        { value: "Medewerker" }
       ],
-      prompt: 'Citybot proberen?',
-      retryPrompt: lang.getStringFor(lang.NOT_UNDERSTOOD_USE_BUTTONS),
+      prompt: "Citybot proberen?",
+      retryPrompt: lang.getStringFor(lang.NOT_UNDERSTOOD_USE_BUTTONS)
     });
   }
 
@@ -67,18 +67,18 @@ export default class QuestionDialog extends WaterfallDialog {
     if (step.context.activity.text === ConfirmTypes.NEGATIVE) {
       // ? user does not want to try
       await step.context.sendActivity(
-        'Geen probleem. Misschien een volgende keer.' +
-          ' U kunt steeds hier terecht met uw vragen. Prettige dag verder. 👏',
+        "Geen probleem. Misschien een volgende keer." +
+          " U kunt steeds hier terecht met uw vragen. Prettige dag verder. 👏"
       );
       await step.endDialog();
     } else if (step.context.activity.text === ConfirmTypes.POSITIVE) {
       // ? user wants to try
-      await step.prompt('confirm_prompt', {
-        choices: [{ value: 'Ja graag' }, { value: 'Neen ik begrijp het' }],
+      await step.prompt("confirm_prompt", {
+        choices: [{ value: "Ja graag" }, { value: "Neen ik begrijp het" }],
         prompt: `Wenst u even te zien op welke manier vragen gesteld kunnen worden? `,
-        retryPrompt: lang.getStringFor(lang.NOT_UNDERSTOOD_USE_BUTTONS),
+        retryPrompt: lang.getStringFor(lang.NOT_UNDERSTOOD_USE_BUTTONS)
       });
-    } else if (step.context.activity.text === 'Medewerker') {
+    } else if (step.context.activity.text === "Medewerker") {
       // ? User wants to talk to a person
       await this.handleEmployee(step.context);
       return await step.endDialog();
@@ -86,16 +86,16 @@ export default class QuestionDialog extends WaterfallDialog {
   }
 
   private async handleUserWantsExample(step: WaterfallStepContext) {
-    if (step.context.activity.text === 'Ja graag') {
+    if (step.context.activity.text === "Ja graag") {
       // tslint:disable:max-line-length
       await step.context.sendActivity(
         `Een voorbeeldvraag zou kunnen zijn:
-"Welke beslissingen werden in de Gemeenteraad genomen omtrent de vernieuwing van onze sportterreinen?" `,
+"Welke beslissingen werden in de Gemeenteraad genomen omtrent de vernieuwing van onze sportterreinen?" `
       );
       // tslint:enable:max-line-length
-      await step.context.sendActivity('Stel gerust je vraag. 🤖');
-    } else if (step.context.activity.text === 'Neen ik begrijp het') {
-      await step.context.sendActivity('Stel gerust je vraag.');
+      await step.context.sendActivity("Stel gerust je vraag. 🤖");
+    } else if (step.context.activity.text === "Neen ik begrijp het") {
+      await step.context.sendActivity("Stel gerust je vraag.");
     }
   }
 
@@ -103,12 +103,9 @@ export default class QuestionDialog extends WaterfallDialog {
     // ? Send the documents
     await step.context.sendActivity(lang.getStringFor(lang.WAIT_WHILE_FETCH));
 
-    await step.context.sendActivity({
-      type: ActivityTypes.Typing,
-    });
     const resolved: QueryResponse.QueryResponse = await this.api.query(
       step.context.activity.text,
-      this.options,
+      this.options
     );
 
     // ? break when no documents were found
@@ -124,20 +121,20 @@ export default class QuestionDialog extends WaterfallDialog {
 
     // ? ask if concept is correct
     if (!resolved.conceptsOfQuery) {
-      console.log('no concepts, skipping question');
+      console.log("no concepts, skipping question");
       await step.next();
       return await this.handleConcept(step, true);
     }
 
     await this.waitFor(step, async () => {
       const formatConcepts = (conceptsArray: string[]) =>
-        conceptsArray.map(concept => conceptMapping(concept)).join(', ');
-      await step.prompt('confirm_prompt', {
+        conceptsArray.map(concept => conceptMapping(concept)).join(", ");
+      await step.prompt("confirm_prompt", {
         prompt: lang
           .getStringFor(lang.ASK_CORRECT_CONCEPTS)
-          .replace('%1%', formatConcepts(resolved.conceptsOfQuery || [])),
+          .replace("%1%", formatConcepts(resolved.conceptsOfQuery || [])),
         retryPrompt: lang.getStringFor(lang.NOT_UNDERSTOOD_USE_BUTTONS),
-        choices: [ConfirmTypes.POSITIVE, ConfirmTypes.NEGATIVE, 'Medewerker'],
+        choices: [ConfirmTypes.POSITIVE, ConfirmTypes.NEGATIVE, "Medewerker"]
       });
     });
   }
@@ -146,7 +143,7 @@ export default class QuestionDialog extends WaterfallDialog {
     const answer = step.context.activity.text;
     if (answer === ConfirmTypes.POSITIVE || skipped) {
       const resolved: QueryResponse.QueryResponse = await this.docsAccessor.get(
-        step.context,
+        step.context
       );
       await step.context
         .sendActivity(`Dit is de relevante info die ik heb gevonden in
@@ -166,26 +163,26 @@ de notulen van de Gemeenteraad. U kan de bestanden downloaden door op de knop te
                   `${desc}...`,
                   this.options.cardUrl,
                   {
-                    type: 'postback',
-                    title: 'Download pdf',
+                    type: "postback",
+                    title: "Download pdf",
                     payload: JSON.stringify({
-                      type: 'download',
+                      type: "download",
                       value: {
-                        uuid: doc.resourceURI,
-                      },
-                    }),
+                        uuid: doc.resourceURI
+                      }
+                    })
                   },
                   {
-                    type: 'postback',
-                    title: 'Paragraaf',
+                    type: "postback",
+                    title: "Paragraaf",
                     payload: JSON.stringify({
-                      type: 'highlight',
+                      type: "highlight",
                       value: {
-                        uuid: doc.resourceURI,
-                      },
-                    }),
-                  },
-                ),
+                        uuid: doc.resourceURI
+                      }
+                    })
+                  }
+                )
               );
             }
           });
@@ -193,7 +190,7 @@ de notulen van de Gemeenteraad. U kan de bestanden downloaden door op de knop te
           await step.context.sendActivity(fbCardBuilder.getData());
         } else {
           await step.context.sendActivity(
-            'Ik je vraag begrepen, maar geen documenten gevonden.',
+            "Ik je vraag begrepen, maar geen documenten gevonden."
           );
         }
       } else {
@@ -210,38 +207,38 @@ de notulen van de Gemeenteraad. U kan de bestanden downloaden door op de knop te
                 [{ url: encodeURI(this.options.cardUrl) || undefined }],
                 [
                   {
-                    type: 'messageBack',
-                    title: 'download document',
+                    type: "messageBack",
+                    title: "download document",
                     value: JSON.stringify({
-                      type: 'download',
-                      value: { uuid: document.resourceURI },
-                    }),
+                      type: "download",
+                      value: { uuid: document.resourceURI }
+                    })
                   },
                   {
-                    type: 'messageBack',
-                    title: 'Paragraaf',
+                    type: "messageBack",
+                    title: "Paragraaf",
                     value: JSON.stringify({
-                      type: 'highlight',
-                      value: { uuid: document.resourceURI },
-                    }),
-                  },
-                ],
+                      type: "highlight",
+                      value: { uuid: document.resourceURI }
+                    })
+                  }
+                ]
               );
             }
-          },
+          }
         );
         if (cards.length > 0) {
           await step.context.sendActivity(MessageFactory.carousel(cards));
         } else {
           await step.context.sendActivity(
-            'Ik je vraag begrepen, maar geen documenten gevonden.',
+            "Ik je vraag begrepen, maar geen documenten gevonden."
           );
         }
       }
-      await step.prompt('confirm_prompt', {
-        prompt: 'Hebt u gevonden wat u zocht?',
+      await step.prompt("confirm_prompt", {
+        prompt: "Hebt u gevonden wat u zocht?",
         retryPrompt: lang.getStringFor(lang.NOT_UNDERSTOOD_USE_BUTTONS),
-        choices: [ConfirmTypes.POSITIVE, ConfirmTypes.NEGATIVE, 'Medewerker'],
+        choices: [ConfirmTypes.POSITIVE, ConfirmTypes.NEGATIVE, "Medewerker"]
       });
     } else if (answer === ConfirmTypes.NEGATIVE) {
       await step.context.sendActivity(lang.getStringFor(lang.REPHRASE));
@@ -249,7 +246,7 @@ de notulen van de Gemeenteraad. U kan de bestanden downloaden door op de knop te
       if (step.context.activity.channelId !== ChannelId.Facebook) {
         await step.beginDialog(QuestionDialog.ID);
       }
-    } else if (answer === 'Medewerker') {
+    } else if (answer === "Medewerker") {
       await this.handleEmployee(step.context);
       if (step.context.activity.channelId !== ChannelId.Facebook) {
         await step.endDialog();
@@ -277,7 +274,7 @@ de notulen van de Gemeenteraad. U kan de bestanden downloaden door op de knop te
       // if (step.context.activity.channelId !== ChannelId.Facebook) {
       await step.beginDialog(QuestionDialog.ID);
       // }
-    } else if (answer === 'Medewerker') {
+    } else if (answer === "Medewerker") {
       await this.handleEmployee(step.context);
       if (step.context.activity.channelId !== ChannelId.Facebook) {
         await step.endDialog();
@@ -290,7 +287,7 @@ de notulen van de Gemeenteraad. U kan de bestanden downloaden door op de knop te
 
   public async sendHighlight(dialogContext: DialogContext, uuid: string) {
     const resolvedDocs: QueryResponse.QueryResponse = await this.docsAccessor.get(
-      dialogContext.context,
+      dialogContext.context
     );
     const chosenDoc = resolvedDocs.documents.find(d => d.resourceURI === uuid);
 
@@ -301,26 +298,26 @@ de notulen van de Gemeenteraad. U kan de bestanden downloaden door op de knop te
 
   public async sendFile(
     dialogContext: DialogContext,
-    uuid: string,
+    uuid: string
   ): Promise<any> {
     const resourceUri: string = uuid;
 
-    console.log('downloading');
+    console.log("downloading");
     const ret = await this.api.downloadFile(resourceUri, this.options);
 
     const filedata = readFileSync(`./downloads/${ret.filename}`);
-    const base64file = Buffer.from(filedata).toString('base64');
+    const base64file = Buffer.from(filedata).toString("base64");
 
     if (dialogContext.context.activity.channelId === ChannelId.Facebook) {
       const fd = new FormData();
-      fd.append('filedata', ret.buffer, {
+      fd.append("filedata", ret.buffer, {
         filename: ret.filename,
-        contentType: ret.contentType,
+        contentType: ret.contentType
       });
       // curl -H "Max-Downloads: 1" -H "Max-Days: 5"
       // --upload-file ./hello.txt https://transfer.sh/hello.txt
       await dialogContext.context.sendActivity(
-        `Ik stuur je de downloadlink onmiddelijk door.`,
+        `Ik stuur je de downloadlink onmiddelijk door.`
       );
       try {
         const res = await CloudinaryApi.upload(`./downloads/${ret.filename}`);
@@ -343,9 +340,9 @@ de notulen van de Gemeenteraad. U kan de bestanden downloaden door op de knop te
         {
           name: ret.filename,
           contentUrl: `data:${ret.contentType};base64,${base64file}`,
-          contentType: ret.contentType,
-        },
-      ],
+          contentType: ret.contentType
+        }
+      ]
     };
 
     return await dialogContext.context.sendActivity(reply);
@@ -354,25 +351,22 @@ de notulen van de Gemeenteraad. U kan de bestanden downloaden door op de knop te
   private async handleEmployee(context: TurnContext) {
     await context.sendActivity(
       `Uw vragen worden doorgestuurd naar een medewerker van uw stad of gemeente.
-Prettige dag verder ☀️`,
+Prettige dag verder ☀️`
     );
   }
 
   private async waitFor(step: DialogContext, cb: Function): Promise<any> {
-    await step.context.sendActivity({
-      type: ActivityTypes.Typing,
-    });
     return new Promise(resolve => {
       // wait 1 to 2 secs for natural feeling
       setTimeout(() => {
         resolve(cb());
-      },         Math.random() * 1000 + 1000);
+      }, Math.random() * 1000 + 1000);
     });
   }
 
   private getBestParagraphForDoc(doc: QueryResponse.Document): string {
     const td = new Turndown();
-    let highlight = '';
+    let highlight = "";
     if (doc.paragraphs && doc.paragraphs.length > 0) {
       highlight = td.turndown(doc.paragraphs[0].content);
     } else {
